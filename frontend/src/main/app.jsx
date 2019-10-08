2bf8a9edad5169c12f69a99e8a769de0c70c857b
Vinicius MRS <vinicius@mrseditorial.cloud>
2019-10-08 17:13:58 -0300

    Configured Task Notification

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -5,10 +5,12 @@ import Header from '../common/template/header'
 import Sidebar from '../common/template/sidebar'
 import Footer from '../common/template/footer'
 
+import Messages from '../common/messages/messages'
 import Routes from './routes'
 
 export default props => (
   <div className='wrapper'>
+    <Messages />
     <Header />
     <Sidebar />
     <div className='content-wrapper'>
