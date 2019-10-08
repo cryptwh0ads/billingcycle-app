3de928d4c41fa4b5102031d5c22b2c6a41893884
Vinicius <vinicius@wh0adsconsultoria.net>
2019-10-07 21:29:19 -0300

    Finished backend

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const allowCors = require('./cors');
+const queryParser = require('express-query-int');
 
 const port = 3333;
 const server = express();
@@ -8,6 +9,7 @@ const server = express();
 server.use(allowCors);
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(queryParser());
 
 server.listen(port, () => {
   console.log(`BACKEND is running on port ${port}!`);
