83f133b784d71208664aee63be6201586b1ad31f
Vinicius <vinicius@wh0adsconsultoria.net>
2019-10-07 21:26:59 -0300

    Configured CORS

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,12 +1,16 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const allowCors = require('./cors');
 
 const port = 3333;
 const server = express();
 
+server.use(allowCors);
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
-server.listen(port,() => { console.log(`BACKEND is running on port ${port}!`)});
+server.listen(port, () => {
+  console.log(`BACKEND is running on port ${port}!`);
+});
 
-module.exports = server
+module.exports = server;
