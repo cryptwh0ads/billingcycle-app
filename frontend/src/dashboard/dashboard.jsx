ae27d2b3dfa5fed870c9e34ab1909b7b465536fb
Vinicius MRS <vinicius@mrseditorial.cloud>
2019-10-08 09:57:40 -0300

    Configured dashboard with valueBox

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,19 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import ContentHeader from '../common/template/contentHeader'
-import Content from '../common/template/content'
+import ContentHeader from "../common/template/contentHeader";
+import Content from "../common/template/content";
+import ValueBox from "../common/widget/valueBox";
+import Row from "../common/layout/row";
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
-        <ContentHeader title='Dashboard' small='Versao 0.1' />
-        <Content>
-          Dashboard
-        </Content>
+        <ContentHeader title="Dashboard" small="Versao 0.1" />
+        <Row>
+          <Content>
+            <ValueBox
+              cols="12 4"
+              color="green"
+              icon="bank"
+              value="R$ 10"
+              text="Total de Creditos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="red"
+              icon="credit-card"
+              value="R$ 10"
+              text="Total de Débitos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="blue"
+              icon="money"
+              value="R$ 0"
+              text="Total Consolidado"
+            />
+          </Content>
+        </Row>
       </div>
-    )
+    );
   }
 }
 
-export default Dashboard
+export default Dashboard;
