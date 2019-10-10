dae21afb9e8b4110a0ce96f765a62fc26d381be4
Vinicius <vinicius@wh0adsconsultoria.net>
2019-10-09 21:26:53 -0300

    Create summary of sum of credits/debts

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -5,10 +5,21 @@ import { connect } from 'react-redux'
 import LabelAndInput from '../common/form/labelAndInput'
 import { init } from './billingCycleActions'
 import ItemList from './itemList'
+import Summary from './summary'
 
 class BillingCycleForm extends Component {
+
+  calculateSummary() {
+    const sum = (t, v) => t + v
+    return {
+      sumOfCredits : this.props.credits.map(c => +c.value || 0).reduce(sum),
+      sumOfDebits: this.props.debits.map(d => +d.value || 0).reduce(sum)
+    }
+  }
+
   render() {
     const { handleSubmit, readOnly, credits, debits } = this.props
+    const { sumOfCredits, sumOfDebits} = this.calculateSummary()
     // console.log(handleSubmit)
     return (
       <div>
@@ -17,6 +28,7 @@ class BillingCycleForm extends Component {
             <Field name='name' component={LabelAndInput} label='Nome' cols='12 4' placeholder='Informe o nome' readOnly={readOnly} />
             <Field name='month' component={LabelAndInput} label='Mes' cols='12 4' placeholder='Informe o mes' readOnly={readOnly} />
             <Field name='year' component={LabelAndInput} label='Ano' cols='12 4' placeholder='Informe o ano' readOnly={readOnly} />
+            <Summary credit={sumOfCredits} debit={sumOfDebits}/>
             <ItemList cols='12 6' list={credits} readOnly={readOnly} field='credits' legend='Creditos' />
             <ItemList cols='12 6' list={debits} readOnly={readOnly} field='debits' legend='Debitos' showStatus={true} />
           </div>
---
+++ frontend/src/billingCycle/summary.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+import Grid from '../common/layout/grid'
+import Row from '../common/layout/row'
+import ValueBox from '../common/widget/valueBox'
+
+export default ({credit, debit}) => (
+  <Grid cols='12'>
+    <fieldset>
+      <legend>Resumo</legend>
+      <Row>
+            <ValueBox
+              cols="12 4"
+              color="green"
+              icon="bank"
+              value={`R$ ${credit}`}
+              text="Total de Creditos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="red"
+              icon="credit-card"
+              value={`R$ ${debit}`}
+              text="Total de Débitos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="blue"
+              icon="money"
+              value={`R$ ${credit - debit}`}
+              text="Total Consolidado"
+            />
+        </Row>
+    </fieldset>
+  </Grid>
+)
